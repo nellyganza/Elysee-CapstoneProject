6ce918d77655f190e235dd8b54f0b0c5ad7a4191
nellyganza <nishimwelys@gmail.com>
2020-09-06 08:07:58 +0200

    FT(User Landing page) With The FOllowing :
    - first page of website (index.html)
    - In content there is my Profession
    - Its linked with other pages that are not yet built
    - there is a css file for styling
    - there a js file for responsiveness
    - there is a font awesome styles from icons
    
    [Done]

---
+++ Templates/js/script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("topnav");
+    console.log(x)
+    if (x.className === "topnavclass") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnavclass";
+    }
+}
+
+function showoption(){
+    var op = document.getElementById('user-option');
+    var ulElement = document.querySelector( "#user-option ul" );
+    var style = document.querySelector( "#user-option ul" ).style.display;
+    if(style=="none")
+        ulElement.style.display = "inline";
+    else
+        ulElement.style.display = "none";
+}
