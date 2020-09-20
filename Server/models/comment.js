2920f96c80327ffb3a727ce6bce4a39af14745c3
nellyganza <nishimwelys@gmail.com>
2020-09-20 03:07:06 +0200

    Ft Comment Blog :
    - Endpoint to Comment on Blog
    
    [Done]

---
+++ Server/models/comment.js
@@ -0,0 +1,20 @@
+import mongoose,{Schema} from 'mongoose';
+const commentSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },  
+    comment : {
+        type: String,
+        required: true
+    }, 
+    // blog: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     required: true,
+    //     ref: 'Blog'
+    // }
+},{timestamps:true})
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
