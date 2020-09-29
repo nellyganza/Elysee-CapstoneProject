19adbfa6a68ea4d6418aa94057d34169f2ea4fcb
nellyganza <nishimwelys@gmail.com>
2020-09-29 17:30:17 +0200

    This is a develop branch for testing my application

--- Server/routes/contactRouters.js
+++ Server/routes/contactRouters.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import contactController from '../controllers/contactController';
+import express from 'express'
+import contactController from '../controllers/contactController'
 import verifyToken from '../middleware/auth'
 
-const contactRouter = express.Router();
+const contactRouter = express.Router()
 
-contactRouter.post('/api/v1/contacts',verifyToken, contactController.save)
+contactRouter.post('/api/v1/contacts', verifyToken, contactController.save)
 contactRouter.get('/api/v1/contacts', contactController.getAll)
 
-
-export default contactRouter;
+export default contactRouter
