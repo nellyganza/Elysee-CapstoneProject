2da1808853f3faea28ce15941dcce78c4db461a0
nellyganza <nishimwelys@gmail.com>
2020-09-29 11:30:52 +0200

    changes on getBlogsUser.js in UI

--- Templates/js/getBlogsUser.js
+++ Templates/js/getBlogsUser.js
@@ -1,94 +1,83 @@
-/* eslint-disable no-undef */
 // Display of Blog
 
-// window.onload = function getBlog(){
-//     var ref =  firebase.database().ref('Blog');
-//     ref.on('value',getData,errorData);
-//  }
-
-fetch('http://localhost:3500/api/v1/blogs')
-	.then((res) => res.json())
-	.then((res) => {
-		console.log(res)
-		res.data.blogs.forEach((element) => {
-			console.log(element.photo.data)
-			//  setImg(element.photo.data);
-			getData(element)
-		})
-	})
+window.onload = function getBlog() {
+	const ref = firebase.database().ref('Blog')
+	ref.on('value', getData, errorData)
+}
 
 function getData(data) {
-	const blogs = data
+	const blogs = data.val()
 	const keys = Object.keys(blogs)
-	console.log('Keys ==>', keys)
-	const id = blogs._id
-	const title = blogs.Title
-	const desc = blogs.Description
-	const date = blogs.createdAt
-	const intro = blogs.Introduction
-	const cont = blogs.Content
-	const imgurl = blogs.photo.data
-	console.log(id, title, desc, date, intro, cont, imgurl)
-	addblog(id, title, desc, date, intro, cont, setImg(imgurl))
+	console.log(keys)
+	for (let i = 0; i < keys.length; i++) {
+		const k = keys[i]
+		const title = blogs[k].Title
+		const desc = blogs[k].Descripttion
+		const date = blogs[k].Date
+		const intro = blogs[k].Introduction
+		const cont = blogs[k].Content
+
+		addblog(k, title, desc, date, intro, cont)
+	}
 }
 function errorData(error) {
 	console.log(error.message)
 }
 
-function addblog(id, title, desc, date, intro, cont, imgUrl) {
-	console.log('Img URL ==>', imgUrl)
-	//  firebase.storage().ref('BlogImage/' +k+'/blog.jpg').getDownloadURL().then(imgUrl =>{
-	const otherblogcontainer = document.getElementById('other-blog')
-	console.log(otherblogcontainer)
-	const figure = document.createElement('figure')
-	figure.classList.add('single-blog')
-	figure.setAttribute('onclick', 'singleBlog(event)')
-	otherblogcontainer.appendChild(figure)
-	const img = document.createElement('img')
-	img.setAttribute('src', imgUrl)
-	img.setAttribute('width', '100%')
-	img.setAttribute('height', '200px')
-	img.setAttribute('alt', 'This is The Image of the Blog post')
-	figure.appendChild(img)
-	const figcap = document.createElement('figcaption')
-	figure.appendChild(figcap)
-	const h1 = document.createElement('h1')
-	h1.classList.add('title')
-	h1.innerHTML = title
-	figcap.appendChild(h1)
-	const h3 = document.createElement('h3')
-	figcap.appendChild(h3)
-	const p1 = document.createElement('p')
-	p1.classList.add('desc')
-	p1.value = desc
-	const p2 = document.createElement('p')
-	p2.classList.add('date')
-	p2.value = date
-	h3.append(`${p1.value} , ${p2.value}`)
-	const p = document.createElement('p')
-	p.width = '100%'
-	figcap.appendChild(p)
-	p.innerText = intro
-	const pp = document.createElement('p')
-	pp.setAttribute('hidden', true)
-	pp.innerText = cont
-	figcap.appendChild(pp)
+function addblog(k, title, desc, date, intro, cont) {
+	firebase.storage().ref(`BlogImage/${k}/blog.jpg`).getDownloadURL().then((imgUrl) => {
+		const otherblogcontainer = document.getElementById('other-blog')
+		console.log(otherblogcontainer)
+		const figure = document.createElement('figure')
+		figure.classList.add('single-blog')
+		figure.setAttribute('onclick', 'singleBlog(event)')
+		otherblogcontainer.appendChild(figure)
+		const img = document.createElement('img')
+		img.setAttribute('src', imgUrl)
+		img.setAttribute('width', '100%')
+		img.setAttribute('height', '200px')
+		img.setAttribute('alt', 'This is The Image of the Blog post')
+		figure.appendChild(img)
+		const figcap = document.createElement('figcaption')
+		figure.appendChild(figcap)
+		const h1 = document.createElement('h1')
+		h1.classList.add('title')
+		h1.innerHTML = title
+		figcap.appendChild(h1)
+		const h3 = document.createElement('h3')
+		figcap.appendChild(h3)
+		const p1 = document.createElement('p')
+		p1.classList.add('desc')
+		p1.value = desc
+		const p2 = document.createElement('p')
+		p2.classList.add('date')
+		p2.value = date
+		h3.append(`${p1.value} , ${p2.value}`)
+		const p = document.createElement('p')
+		p.width = '100%'
+		figcap.appendChild(p)
+		p.innerText = intro
+		const pp = document.createElement('p')
+		pp.setAttribute('hidden', true)
+		pp.innerText = cont
+		figcap.appendChild(pp)
 
-	const h5 = document.createElement('h5')
-	h5.setAttribute('hidden', true)
-	h5.innerText = id
-	figcap.appendChild(h5)
-	//  }).catch(error=>{
-	//      console.log(error);
-	//  });
-	motionPicture()
+		const h5 = document.createElement('h5')
+		h5.setAttribute('hidden', true)
+		h5.innerText = k
+		figcap.appendChild(h5)
+	})
+		.catch((error) => {
+			console.log(error)
+		})
+	test()
 }
 let i = 0
-const tid = setInterval(motionPicture, 5000)
-function motionPicture() {
+const tid = setInterval(test, 5000)
+function test() {
 	try {
 		const x = document.getElementsByClassName('single-blog')
-		if (i == x.length) { i = 0 }
+		if (i == x.length) i = 0
 		const y = x[i].lastChild
 		const blo = document.getElementById('main-blog')
 		blo.firstElementChild.innerHTML = y.firstChild.innerHTML
--- Templates/js/singleblog.js
+++ Templates/js/singleblog.js
@@ -11,31 +11,22 @@ var getParams = function (url) {
     return params;
 };
 var params =getParams(document.URL);
+var singleblog = document.getElementById('single-blog');
+var img = params["img"];
+singleblog.querySelector('img').src = img;
+var title = params["title"];
+singleblog.querySelector('h1').innerText = title;
+var desc = params["desc"];
+singleblog.querySelectorAll('p')[0].innerText = desc;
+var intro = params["intro"];
+singleblog.querySelectorAll('p')[1].innerText = intro;
+var cont = params["cont"];
+singleblog.querySelectorAll('p')[2].innerText = cont;
 var id = params["id"];
-fetch(`http://localhost:3500/blogs/${id}`)
- .then(res => res.json())
- .then(res =>{
-   console.log(res);
-     var singleblog = document.getElementById('single-blog');
-     var img = setImg(res.data.blog.photo.data);
-     singleblog.querySelector('img').src = img;
-     var title = res.data.blog.Title;
-     singleblog.querySelector('h1').innerText = title;
-     var desc = res.data.blog.Description;
-     singleblog.querySelectorAll('p')[0].innerText = desc;
-     var intro = res.data.blog.Introduction;
-     singleblog.querySelectorAll('p')[1].innerText = intro;
-     var cont = res.data.blog.Content;
-     singleblog.querySelectorAll('p')[2].innerText = cont;
- });
-
-
-// var id = params["id"];
-// console.log(id);
+console.log(id);
 
 
 function commentBlog() {
-    if(signedUser()){
     var comment = document.getElementById('textareacomment').value;
     if(comment!=""){
         firebase.database().ref('Comment/'+id+`/${new Date()}`).set({
@@ -57,11 +48,7 @@ function commentBlog() {
     {
         console.log("no Comment");
     }
-    }
-    else
-    {
-        document.getElementById('id01').style.display= 'block';
-    }
+
 }
 
 window.onload = function getComments(){
