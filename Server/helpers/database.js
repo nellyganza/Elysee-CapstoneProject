d638bf138b5d9cfc0424ad1d4ca4c803dfe53a40
nellyganza <nishimwelys@gmail.com>
2020-09-30 10:36:42 +0200

    fixing DB URL

--- Server/helpers/database.js
+++ Server/helpers/database.js
@@ -1,13 +1,7 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
-let URL
-if (process.env.NODE_ENV === 'test') {
-	URL = process.env.DB_TEST_URL
-} else {
-	URL = process.env.DB_URL
-}
-// process.env.NODE_ENV === 'test' ? process.env.DB_TEST_URL : process.env.DB_URL
+const URL = process.env.NODE_ENV === 'test' ? process.env.DB_TEST_URL : process.env.DB_URL
 mongoose.connect(URL, {
 	// useMongoClient:true,
 	useNewUrlParser: true,
