b1e4f74c9b0148d31dcae685df967601212fdc26
nellyganza <nishimwelys@gmail.com>
2020-09-30 10:49:26 +0200

    Conole log URL to see its value on travis

--- Server/helpers/database.js
+++ Server/helpers/database.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 
 const URL = process.env.NODE_ENV === 'test' ? process.env.DB_TEST_URL : process.env.DB_URL
+console.log(URL, typeof URL)
 mongoose.connect(URL, {
 	// useMongoClient:true,
 	useNewUrlParser: true,
